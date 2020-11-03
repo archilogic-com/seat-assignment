98fabde6f2556e0147ef160cb72f166d90989653
David Hojman <david.hojman@lagarsoft.com>
2020-11-02 23:17:04 -0300

    warning added

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import axios from 'axios'
+import Media from 'react-media'
 
 axios.interceptors.request.use((config) => {
     config.params = config.params || {};
@@ -14,7 +15,24 @@ axios.interceptors.request.use((config) => {
     return Promise.reject(error);
 });
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+
+<>
+    <Media query="(max-width: 500px)" render={() =>
+        (
+          <div><img className='device-not-supported' src={require("./warning.png")} alt="" /></div>
+        )}
+      />
+      <Media query="(min-width: 501px)" render={() =>
+        (
+          <App />
+        )}
+      />
+
+</>, document.getElementById('root'))
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
